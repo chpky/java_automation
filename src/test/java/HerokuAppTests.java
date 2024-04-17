90b0157dee16aa1890fddbdbd80b1b486bf42db6
Yaroslav Radzievskiy <radzievskiyaroslav@gmail.com>
2024-04-17 14:37:45 +0300

    added tests for herokuapp, homework 4

---
+++ src/test/java/HerokuAppTests.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+import static utils.TestConfig.SetConfig;
+
+public class HerokuAppTests {
+
+    @BeforeAll
+    static void SetUp() {
+        SetConfig("https://the-internet.herokuapp.com");
+    }
+
+    @Test
+    void DragNDropWithActionsTest() {
+        open("/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void DragNDropWithSelenideTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
