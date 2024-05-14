1229cdadd78a6d0caa3f2a5f30908caceb8735ab
Yaroslav Radzievskiy <radzievskiyaroslav@gmail.com>
2024-05-14 22:28:05 +0300

    complete tasks 1, 2

---
+++ java-core/src/main/java/ru/qa/school/core/OddTask.java
@@ -0,0 +1,12 @@
+package ru.qa.school.core;
+
+public class OddTask {
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(2));
+    }
+}
